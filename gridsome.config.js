bb1678d3b00e91949d9e11118e31c844bd47ea02
metaroot <faisal.ahmed.cse.sust@gmail.com>
2021-06-17 09:27:20 +0600

    Do madrassa frontend

---
+++ gridsome.config.js
@@ -0,0 +1,30 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'অনলাইন মাদ্রাসায়',
+  siteUrl: 'https://metaroot.github.io',
+  pathPrefix: '/nadir-faisal-online-madrassa',
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "articles/**/*.md",
+        typeName: "Article",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    },
+  ],
+  transformers: {
+    remark: {
+      plugins: ["@gridsome/remark-prismjs"]
+    }
+  }
+}
