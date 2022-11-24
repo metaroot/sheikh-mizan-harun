cce60b90d4dfa25e110abbe13cce17a4c18ccec7
mlab-faisal-ahmed <faisal.ahmed@monstar-lab.com>
2022-11-24 10:33:42 +0600

    first commit

---
+++ data/headerNavLinks.js
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'লেখালেখি' },
+  { href: '/tags', title: 'বিষয়' },
+  { href: '/projects', title: 'বই ও আলোচনা' },
+  { href: '/about', title: 'পরিচিতি' },
+]
+
+export default headerNavLinks
---
+++ pages/projects.js
@@ -0,0 +1,35 @@
+import siteMetadata from '@/data/siteMetadata'
+import projectsData from '@/data/projectsData'
+import Card from '@/components/Card'
+import { PageSEO } from '@/components/SEO'
+
+export default function Projects() {
+  return (
+    <>
+      <PageSEO title={`বই ও আলোচনা - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            বই ও আলোচনা
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            শাইখ মীযান হারুনের বই, কোর্সসহ অন্যান্য খেদমতসমূহ
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
